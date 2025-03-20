760906d74d072593db024d00a750561a7406824d
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2025-03-20 13:51:46 +0100

    Première version qui fonctionne

---
+++ @rd/2.INAMotCleVideo/main.py
@@ -0,0 +1,91 @@
+import requests
+from pytube import YouTube
+from moviepy.editor import VideoFileClip
+import whisper
+from transformers import (
+    TokenClassificationPipeline,
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+)
+from transformers.pipelines import AggregationStrategy
+import numpy as np
+
+def download_video(url, output_path='video.mp4'):
+    # Vérifier si c'est une URL de Youtube
+    if "youtube.com" in url or "youtu.be" in url:
+        # Crée un objet YouTube (en utilisant la librairie YouTube de pytube) à partir de l'URL
+        # Les objets YouTube permettent d'accéder aux différents formats et qualités de téléchargement disponibles (streams).
+        yt = YouTube(url)
+        # Filtrer les flux de la vidéo pour choisir le format MP4
+        stream = yt.streams.filter(file_extension='mp4').first()
+        # Télécharge la première vidéo correspondant aux critères et la sauvegarde dans le dossier du projet sous video.mp4
+        stream.download(filename=output_path)
+    else:
+        # Effectue une requête HTTP GET pour récupérer le contenu
+        response = requests.get(url)
+        # Ouvre un fichier en mode écriture binaire ('wb')
+        # Enregistre le contenu de la réponse HTTP
+        # f correspond au fichier ouvert
+        with open(output_path, 'wb') as f:
+            # Écrit les données de response dans le fichier ouvert
+            f.write(response.content)
+
+def extract_audio(video_path, audio_path='audio.mp3'):
+    # Charger un fichier vidéo sous forme d’objet manipulable en Python.
+    video = VideoFileClip(video_path)
+    video.audio.write_audiofile(audio_path)
+
+def transcribe_audio(audio_path):
+    model = whisper.load_model("small")
+    result = model.transcribe(audio_path)
+    return result['text']
+
+# Define keyphrase extraction pipeline
+class KeyphraseExtractionPipeline(TokenClassificationPipeline):
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(
+            # Charge un modèle pré-entraîné pour la classification de tokens
+            # Ici, un token est une unité de texte utilisée par le modèle de traitement du langage
+            model=AutoModelForTokenClassification.from_pretrained(model),
+
+            # Charge le tokenizer correspondant au modèle
+            tokenizer=AutoTokenizer.from_pretrained(model),
+            *args,
+            **kwargs
+        )
+
+    def postprocess(self, all_outputs):
+        results = super().postprocess(
+            all_outputs=all_outputs,
+            aggregation_strategy=AggregationStrategy.SIMPLE,
+        )
+        return np.unique([result.get("word").strip() for result in results])
+
+
+# Classe keyword extractor
+# 1 qui utilise le modèle sur HuggingFace (actuelle)
+# 2 qui utilise un LLM pour lui demander d'extraire des mots clés
+
+def main(video_url):
+    video_path = 'video.mp4'
+    audio_path = 'audio.mp3'
+
+    # Download the video
+    download_video(video_url, video_path)
+
+    # Extract audio from the video
+    extract_audio(video_path, audio_path)
+
+    # Transcribe audio to text
+    text = transcribe_audio(audio_path)
+    print(text)
+
+    model_name = "ml6team/keyphrase-extraction-kbir-inspec"
+    extractor = KeyphraseExtractionPipeline(model=model_name)
+
+    keyphrases = extractor(text)
+    print(keyphrases)
+
+# Example usage
+video_url = 'https://media-hub.ina.fr/video/FX2qHyT1cs80A6MYTUFQLyZufFZ/hl3Ww+TydvrCUf44DXd/nBj5A8M2ExDP0Kd04fTjAJBu2FGHVZQ6AKkYdMEAy5ipWVVE+BYUa95UMRZgla1iVuavkiwGZc8t4vRhySFNQaKsF8sTv1I64yy/2Q==/sl_iv/QD+4g/Dm8yqwue0mJ4w8Gg==/sl_hm/QypS76elrNTiKS9bOAE2xtsGkdG11Vs4++5ocCQR4QTpWKb5pQQIdKFgD9qG5NYL3JlJUolo9zBR2OSTJQH7ZA==/sl_e/1743597232'
+main(video_url)
