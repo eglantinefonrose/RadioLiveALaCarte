5167c300f0a61c4bf69f477a111545ac1504304d
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2025-04-10 09:11:55 +0200

    Création de l'app mobile Test pour FFMPEG depuis l'app

---
+++ @rd/3.FFMPEGOnIOS/Test-FFMPEG/Test-FFMPEG/Test_FFMPEGApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_FFMPEGApp.swift
+//  Test-FFMPEG
+//
+//  Created by Eglantine Fonrose on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Test_FFMPEGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ @rd/3.FFMPEGOnIOS/Test-FFMPEG/Test-FFMPEGTests/Test_FFMPEGTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_FFMPEGTests.swift
+//  Test-FFMPEGTests
+//
+//  Created by Eglantine Fonrose on 10/04/2025.
+//
+
+import Testing
+@testable import Test_FFMPEG
+
+struct Test_FFMPEGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
