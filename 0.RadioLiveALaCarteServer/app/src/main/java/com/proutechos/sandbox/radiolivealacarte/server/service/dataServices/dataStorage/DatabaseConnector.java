fbdc3fecd4c31d906a391b11b8467e4ce2f7a760
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2025-07-15 18:44:43 +0200

    MAJ du chemin de la db (Bug fix)

--- 0.RadioLiveALaCarteServer/app/src/main/java/com/proutechos/sandbox/radiolivealacarte/server/service/dataServices/dataStorage/DatabaseConnector.java
+++ 0.RadioLiveALaCarteServer/app/src/main/java/com/proutechos/sandbox/radiolivealacarte/server/service/dataServices/dataStorage/DatabaseConnector.java
@@ -1,5 +1,6 @@
 package com.proutechos.sandbox.radiolivealacarte.server.service.dataServices.dataStorage;
 
+import java.net.URL;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.sql.Connection;
@@ -8,9 +9,17 @@ import java.sql.DriverManager;
 public class DatabaseConnector {
     public static Connection getConnection() throws Exception {
         // Remonter d’un dossier puis aller dans @db
-        Path dbPath = Paths.get("").toAbsolutePath().getParent().resolve("/dbStorage/RadioLiveALaCarteDB.db");
+
+        Path currentPath = Paths.get("").toAbsolutePath();
+
+        // Répertoire parent
+        Path parentPath = currentPath.getParent();
+
+        // Aller dans le sous-dossier "@db" à partir du répertoire parent
+        Path dbPath = parentPath.resolve("@db/RadioLiveALaCarteDB.db");
 
         String url = "jdbc:sqlite:" + dbPath.toString();
+        System.out.println(url);
         return DriverManager.getConnection(url);
     }
 }
